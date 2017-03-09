e42f22b1ab2bb1797d86f01e11770d8095306ecf
Андрей <@DESKTOP-G3CDARH>
2017-03-09 10:36:02 +0400

    Создал класс: менеджер БД. Частично протестил

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,19 +12,21 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        ManagerBD mbd = new ManagerBD();
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int i= 0;
-            while(true)
-            {
-                i++;
-                MessageBox.Show("" + i);
-            }
+            mbd.Connection();
+            DataTable dt = mbd.selectionquery("Select * from FurnituraZakaz;");
+            var n =  dt.Rows[0][1]; // получаю ячейку
+            textBox1.Text = n.ToString();
+            dataGridView1.DataSource = dt;
+
         }
     }
 }
