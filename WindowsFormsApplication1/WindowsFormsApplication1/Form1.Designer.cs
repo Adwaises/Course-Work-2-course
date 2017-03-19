2f0add6a7a03b6a1e44e99c70d84f670a4d82a28
Андрей <@DESKTOP-G3CDARH>
2017-03-19 09:29:14 +0400

    раскидал по классам

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,17 +33,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -85,38 +81,22 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(424, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(571, 455);
+            this.pictureBox1.Size = new System.Drawing.Size(761, 499);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(424, 473);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(571, 30);
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
-            // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(274, 182);
+            this.pictureBox3.Location = new System.Drawing.Point(377, 144);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(144, 151);
+            this.pictureBox3.Size = new System.Drawing.Size(41, 151);
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Location = new System.Drawing.Point(969, 192);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(184, 56);
-            this.pictureBox4.TabIndex = 7;
-            this.pictureBox4.TabStop = false;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(1036, 344);
+            this.checkBox1.Location = new System.Drawing.Point(1037, 350);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(69, 17);
             this.checkBox1.TabIndex = 8;
@@ -126,7 +106,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(1036, 367);
+            this.checkBox2.Location = new System.Drawing.Point(1037, 373);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(130, 17);
             this.checkBox2.TabIndex = 9;
@@ -135,11 +115,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1036, 412);
+            this.button3.Location = new System.Drawing.Point(1037, 396);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
-            this.button3.Text = "button3";
+            this.button3.Text = "График";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -147,12 +127,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1197, 515);
+            this.ClientSize = new System.Drawing.Size(1197, 631);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -163,9 +141,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,9 +154,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Button button3;
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         ManagerBD mbd = new ManagerBD();
+        Reports report = new Reports();
         public Form1()
         {
             InitializeComponent();
@@ -30,301 +31,21 @@ namespace WindowsFormsApplication1
 
         public void diagr()
         {
-            Dictionary<int, int> dictSum = new Dictionary<int, int>();
-            Dictionary<int, int> dictCount = new Dictionary<int, int>();
-            for (int i=1;i<=12; i++)
-            {
-                dictSum.Add(i,0);
-                dictCount.Add(i, 0);
-            }
 
-            mbd.Connection();
-            DataTable dt = mbd.Statistic();
-
-            for(int i=0; i<dt.Rows.Count;i++)
-            {
-                dictSum[Convert.ToInt32( dt.Rows[i][0])] = Convert.ToInt32(dt.Rows[i][1])/1000;
-                dictCount[Convert.ToInt32(dt.Rows[i][0])] = Convert.ToInt32(dt.Rows[i][2]);
-            }
-            //dictCount[11] = 3; dictCount[12] = 8; dictCount[5] = 2;
-
-
-            int w = pictureBox1.Width;
-            int h = pictureBox1.Height;
-            int maxX = 12;
-            int maxY = 1000;
-            int max = h - 25;
-
-            Bitmap bmp = new Bitmap(w, h);
-            Graphics gr = Graphics.FromImage(bmp);
-            float cdx = (w - 90) / maxX;
-            float cdy = (h - 50) / maxY;
-
-            Pen p = new Pen(Brushes.Green, 3);
-            p.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
-            gr.DrawLine(p, (int)(0 * cdx) + 40, (int)h - 25, (int)(0 * cdx) + 40, 0);
-            gr.DrawLine(p, 0, (int)(max - 0 * cdy), (int)w - 50, (int)(max - 0 * cdy));
-
-            if (checkBox1.Checked) {
-                // сетка
-                for (float x = 0; x < maxX; x++)
-                {
-                    gr.DrawString((x + 1).ToString(), new Font("Arial", 10), Brushes.Green, (int)(x * cdx) + 40, max + 10);
-                    gr.DrawLine(Pens.Green, (int)(x * cdx) + 40, (int)h - 25, (int)(x * cdx) + 40, 0);
-                }
-
-                double cy = 0.4;
-                for (float y = 1; y <= maxY + 1; y += 100)
-                {
-                    gr.DrawString((y - 1).ToString(), new Font("Arial", 10), Brushes.Green, 0, (int)(max - (y) * cy));
-                    gr.DrawLine(Pens.Green, 0, (int)(max - (int)(y * cy)), (int)w - 50, (int)(max - (int)(y * cy)));
-                }
-
-                //гист
-                for (int i = 1; i <= dictSum.Count; i++)
-                {
-                    gr.FillRectangle(Brushes.Red, 40 * (i - 1) + 40, (int)(maxY * 0.43) - (int)(dictSum[i] * 0.4), 10, (int)(dictSum[i] * 0.4));
-                    //gr.DrawString(Convert.ToString(dictCount[i]), new Font("Arial", 10), Brushes.Red,
-                    //40 * (i - 1) + 40, (int)(maxY * 0.43) - (int)(dictSum[i] * 0.4) -20);
-                }
-
-                
-            } else if (checkBox2.Checked)
-            {
-                //сетка
-                for (float x = 0; x < maxX; x++)
-                {
-                    gr.DrawString((x + 1).ToString(), new Font("Arial", 10), Brushes.Green, (int)(x * cdx) + 40, max + 10);
-                    gr.DrawLine(Pens.Green, (int)(x * cdx) + 40, (int)h - 25, (int)(x * cdx) + 40, 0);
-                }
-
-                double cy = 0.4;
-                for (float y = 1; y <= maxY + 1; y += 100)
-                {
-                    gr.DrawString(((y - 1)/100).ToString(), new Font("Arial", 10), Brushes.Green, 0, (int)(max - (y) * cy));
-                    gr.DrawLine(Pens.Green, 0, (int)(max - (int)(y * cy)), (int)w - 50, (int)(max - (int)(y * cy)));
-                }
-
-                for (int i = 1; i <= dictCount.Count; i++)
-                {
-                    gr.FillRectangle(Brushes.Red, 40 * (i - 1) + 40, (int)(maxY * 0.43) - dictCount[i] * 40, 10, (int)(dictCount[i] * 40));
-                    //gr.DrawString(Convert.ToString(dictCount[i]), new Font("Arial", 10), Brushes.Red,
-                    //40 * (i - 1) + 40, (int)(maxY * 0.43) - (int)(dictSum[i] * 0.4) -20);
-                }
-
-            }
-            pictureBox1.Image = bmp;
-
-            w = pictureBox2.Width;
-            h = pictureBox2.Height;
-            Bitmap bmpD = new Bitmap(w, h);
-            Graphics grD = Graphics.FromImage(bmpD);
-
-            grD.DrawString("Месяц", new Font("Arial", 10), Brushes.Black,250,5);
-
-            pictureBox2.Image = bmpD;
-
-            w = pictureBox3.Width;
-            h = pictureBox3.Height;
-            Bitmap bmpL = new Bitmap(w, h);
-            Graphics grL = Graphics.FromImage(bmpL);
-            StringFormat stringFormat = new StringFormat();
-            stringFormat.FormatFlags = StringFormatFlags.DirectionVertical;
             if (checkBox1.Checked)
             {
-                grL.DrawString("Выручка (тыс)", new Font("Arial", 10), Brushes.Black, 50, 120);
-            }else if (checkBox2.Checked)
-            {
-                grL.DrawString("Количество заказов", new Font("Arial", 10), Brushes.Black, 30, 120);
+                pictureBox1.Image = report.diagr(true);
+                pictureBox3.Image = report.vertLabelDiagr(true);
             }
-            bmpL.RotateFlip(RotateFlipType.Rotate270FlipNone);
-            pictureBox3.Image = bmpL;
-
-            w = pictureBox4.Width;
-            h = pictureBox4.Height;
-            Bitmap bmpLeg = new Bitmap(w, h);
-            Graphics grLeg = Graphics.FromImage(bmpLeg);
-            grLeg.DrawRectangle(new Pen(Brushes.Black),0,0,w-1,h-1);
-
-            grLeg.FillRectangle(Brushes.Red, 10, 23, 10, 10);
-            if (checkBox1.Checked)
+            else if (checkBox2.Checked)
             {
-                grLeg.DrawString("Выручка (тыс)", new Font("Arial", 10), Brushes.Black, 35, 20);
-            } else if (checkBox2.Checked)
-            {
-                grLeg.DrawString("Количество заказов", new Font("Arial", 10), Brushes.Black, 35, 20);
+                pictureBox1.Image = report.diagr(false);
+                pictureBox3.Image = report.vertLabelDiagr(false);
             }
-            //grLeg.DrawString("1", new Font("Arial", 10), Brushes.Red, 10, 30);
-            //grLeg.DrawString("Количество заказов", new Font("Arial", 10), Brushes.Black, 35, 30);
-
-
-            pictureBox4.Image = bmpLeg;
-
-            //эксель
+            report.ExcelDiagr();
+            MessageBox.Show("OK");
+        }
             
-            try
-            {
-                Excel.Application ObjExcel = new Excel.Application();
-                Excel.Workbook ObjWorkBook;
-                Excel.Worksheet ObjWorkSheet;
-                ObjWorkBook = ObjExcel.Workbooks.Add(System.Reflection.Missing.Value);
-                ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
-                for(int i = 1; i<=dictSum.Count; i++)
-                {
-                    ObjWorkSheet.Cells[i, 1] = Convert.ToString(dictSum[i]);
-                    ObjWorkSheet.Cells[i, 2] = Convert.ToString(dictCount[i]);
-                }
-                //ObjWorkSheet.Cells[3, 1] = "51";
-                //ObjExcel.Application.DisplayAlerts = true;
-                ObjExcel.DisplayAlerts = false;
-                ObjWorkBook.SaveAs("book.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlExclusive,
-                Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-               
-                ObjExcel.Quit();
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show("Ошибка1 \n" + exc.Message);
-            }
-
-
-            Excel.Application excelapp = new Excel.Application();
-            try
-            {
-
-               // if (checkBox1.Checked){
-                    ExcelDiagr1();
-                //}else if(checkBox2.Checked)
-                //{
-                    ExcelDiagr2();
-                //}
-
-
-                MessageBox.Show("OK");
-
-
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show("Ошибка2 \n" + exc.Message);
-                excelapp.Quit();
-            }
-    
-        }
-
-        public void ExcelDiagr2()
-        {
-            Excel.Application excelapp = new Excel.Application();
-            try
-            {
-
-
-                // excelapp.Visible = true;
-                Excel.Workbooks excelappworkbooks = excelapp.Workbooks;
-                Excel.Workbook excelappworkbook = excelapp.Workbooks.Open("book.xlsx",
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-                Excel.Sheets excelsheets = excelappworkbook.Worksheets;
-
-                //excelappworkbook = excelappworkbooks[2];
-                //excelappworkbook.Activate();
-                //excelsheets = excelapp.ActiveWorkbook.Worksheets;
-
-                Excel.Worksheet excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);
-                excelworksheet.Activate();
-                //Excel.Worksheet excelworksheet = (Excel.Worksheet)excelappworkbook.Sheets[2];
-                Excel.Range excelcells = excelworksheet.get_Range("B1", "B12");
-                excelcells.Select();
-
-                Excel.Chart excelchart = (Excel.Chart)excelapp.Charts.Add(Type.Missing,
-                    Type.Missing, Type.Missing, Type.Missing);
-                excelapp.ActiveChart.HasTitle = true;
-                excelapp.ActiveChart.ChartTitle.Text = "Количество заказов";
-
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlCategory,
-                Excel.XlAxisGroup.xlPrimary)).HasTitle = true;
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlCategory,
-                Excel.XlAxisGroup.xlPrimary)).AxisTitle.Text = "Месяц";
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlValue,
-                Excel.XlAxisGroup.xlPrimary)).HasTitle = true;
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlValue,
-                Excel.XlAxisGroup.xlPrimary)).AxisTitle.Text = "Количество";
-
-                Excel.SeriesCollection seriesCollection =
-                (Excel.SeriesCollection)excelapp.ActiveChart.SeriesCollection(Type.Missing);
-                Excel.Series series = seriesCollection.Item(1);
-                series.Name = "Количество заказов";
-
-                //excelworksheet.Name = "Количество заказов";
-
-                excelapp.DisplayAlerts = false;
-                excelappworkbook.SaveAs("book.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlExclusive,
-               Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-                
-                excelapp.Quit();
-
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show("Ошибка22 \n" + exc.Message);
-                excelapp.Quit();
-            }
-        }
-
-        public void ExcelDiagr1()
-        {
-            Excel.Application excelapp = new Excel.Application();
-            try
-            {
-
-
-                // excelapp.Visible = true;
-                Excel.Workbooks excelappworkbooks = excelapp.Workbooks;
-                Excel.Workbook excelappworkbook = excelapp.Workbooks.Open("book.xlsx",
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
-                Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-                Excel.Sheets excelsheets = excelappworkbook.Worksheets;
-                Excel.Worksheet excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);
-                Excel.Range excelcells = excelworksheet.get_Range("A1", "A12");
-                excelcells.Select();
-                //выручка
-                Excel.Chart excelchart = (Excel.Chart)excelapp.Charts.Add(Type.Missing,
-                    Type.Missing, Type.Missing, Type.Missing);
-                excelapp.ActiveChart.HasTitle = true;
-                excelapp.ActiveChart.ChartTitle.Text = "Выручка";
-
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlCategory,
-                Excel.XlAxisGroup.xlPrimary)).HasTitle = true;
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlCategory,
-                Excel.XlAxisGroup.xlPrimary)).AxisTitle.Text = "Месяц";
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlValue,
-                Excel.XlAxisGroup.xlPrimary)).HasTitle = true;
-                ((Excel.Axis)excelapp.ActiveChart.Axes(Excel.XlAxisType.xlValue,
-                Excel.XlAxisGroup.xlPrimary)).AxisTitle.Text = "Выручка (тыс)";
-
-                Excel.SeriesCollection seriesCollection =
-                (Excel.SeriesCollection)excelapp.ActiveChart.SeriesCollection(
-                Type.Missing);
-                Excel.Series series = seriesCollection.Item(1);
-                series.Name = "Выручка";
-                
-                excelapp.DisplayAlerts = false;
-                excelappworkbook.SaveAs("book.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlExclusive,
-               Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-                excelapp.Quit();
-
-
-
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show("Ошибка2 \n" + exc.Message);
-                excelapp.Quit();
-            }
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             mbd.Connection();
@@ -343,11 +64,11 @@ namespace WindowsFormsApplication1
 
 
 
-            /*
+            
             //отчет
-            Reports report = new Reports();
+            
             report.blank();
-            DialogResult result = MessageBox.Show("Отчет сформирован","Сообщение",MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            DialogResult result = MessageBox.Show("Открыть?", "Отчет сформирован", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
             
             if (result == DialogResult.Yes) //Если нажал Да
             {
@@ -356,7 +77,7 @@ namespace WindowsFormsApplication1
             }
 
             
-            */
+            
 
 
         }
