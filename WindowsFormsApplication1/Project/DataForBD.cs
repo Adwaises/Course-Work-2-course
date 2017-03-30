5782c6b0e89692f420061174377be6be09be1c5f
Andrey <Andrey@DESKTOP-Q1CFNRC>
2017-03-30 18:29:01 +0300

    сделал класс для вывода бд (для тоуз)

--- WindowsFormsApplication1/Project/DataForBD.cs
+++ WindowsFormsApplication1/Project/DataForBD.cs
@@ -9,13 +9,22 @@ namespace Project
     static class DataForBD
     {
         //заказ
-        public static int id_zakaz;
+        public static int idZakaz;
         public static int length;
         public static int width;
         public static int height;
-        public static int id_customer;
+        public static int idCustomer;
+        public static int summa;
+        public static int mounth;
+        //заказчик если нет в таблице (нужно проверить)
+        public static string famil;
+        public static string name;
+        public static string otchestvo;
+        public static string num_tel;
+        public static string mail;
 
-
+        public static List<string> listZakazStroyMat = new List<string>();
+        public static List<string> listZakazMebTeh = new List<string>();
 
     }
 }
--- WindowsFormsApplication1/Project/FormBD.cs
+++ WindowsFormsApplication1/Project/FormBD.cs
@@ -20,21 +20,57 @@ namespace Project
 
             //заполнение стат класса
             //заказ
-            DataForBD.id_zakaz = 2;
+            DataForBD.idZakaz = 2;
             DataForBD.length = 400;
             DataForBD.width = 400;
             DataForBD.height = 250;
+            DataForBD.idCustomer = 1;
+            DataForBD.summa = 75000; // нужно рассчитать
+            DataForBD.mounth = 2;
+            // заказчик если есть
 
-            //
-
+            // список заказ_строймат
+            DataForBD.listZakazStroyMat.Add("2,12");
+            DataForBD.listZakazStroyMat.Add("2,22");
+            // список заказ_мебтех
+            DataForBD.listZakazMebTeh.Add("2,11,1");
+            DataForBD.listZakazMebTeh.Add("2,21,1");
+            DataForBD.listZakazMebTeh.Add("2,21,1");
+            DataForBD.listZakazMebTeh.Add("2,21,1");
+            DataForBD.listZakazMebTeh.Add("2,21,1");
 
         }
 
         private void FormBD_Load(object sender, EventArgs e)
         {
             mbd.Connection();
-            DataTable dt = mbd.selectionquery("select * from Zakaz;");
+            DataTable dt = mbd.selectionquery("select * from zakaz ;");
             dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // вставка в заказ стр мат
+            /*
+            foreach (var n in DataForBD.listZakazStroyMat) {
+                mbd.controlquery("insert into StroyMaterialZakaz values ( "+Convert.ToString(n) +");");
+            }
+            */
+            // вставка в фурнит
+            /*
+            foreach (var n in DataForBD.listZakazMebTeh)
+            {
+                mbd.controlquery("insert into FurnituraZakaz  values ( " + Convert.ToString(n) + ");");
+            }
+            */
+            //вставка в заказ
+            /*
+            mbd.controlquery("insert into Zakaz  values (" + DataForBD.idZakaz.ToString() + ", "+ DataForBD.length.ToString()
+                + " ," + DataForBD.width.ToString() + " ," + DataForBD.height.ToString() + " ," + DataForBD.idCustomer.ToString() 
+                + " ," + DataForBD.mounth.ToString() + " ," + DataForBD.summa.ToString() + ")");
+                */
+            
+
         }
     }
 }
