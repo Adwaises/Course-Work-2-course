0b08c57c0327d7753bea0b78b631f6155e084d65
Andrey <Andrey@DESKTOP-Q1CFNRC>
2017-04-20 16:14:55 +0400

    продукции

--- WindowsFormsApplication1/Project/FormBD.cs
+++ WindowsFormsApplication1/Project/FormBD.cs
@@ -26,7 +26,7 @@ namespace Project
         private void FormBD_Load(object sender, EventArgs e)
         {
             mbd.Connection();
-            DataTable dt = mbd.selectionquery("select * from zakaz ");
+            DataTable dt = mbd.selectionquery("select naimenovanie,count(*) from FurnituraZakaz  join furnitura on furnitura.id_furnit = FurnituraZakaz.id_furnitura group by id_furnitura order by count(*) desc ");
             //DataTable dt = mbd.selectionquery("select * from StroyMaterialZakaz   ");
             //DataTable dt = mbd.selectionquery("select * from FurnituraZakaz    ");
             //DataTable dt = mbd.selectionquery("select sum(summa)from zakaz where month_Zakaz = 4");
